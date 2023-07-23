543ea552e0be5b643d88568e5f6c884d3e58c069
saad696 <shksaad911@gmail.com>
2023-07-23 23:04:42 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
-import { Outlet, redirect, useLocation, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { Button, Space } from 'antd';
 import { useEffect } from 'react';
+import { Footer } from 'antd/es/layout/layout';
+import { GithubFilled } from '@ant-design/icons';
 
 const Navigation = ({ children }) => {
     const navigate = useNavigate();
     let currentLocation = useLocation().pathname.split('/')[1];
-
-    console.log(location);
     return (
         <>
             <div
@@ -40,7 +40,49 @@ const Navigation = ({ children }) => {
                 </Space>
             </div>
 
-            {children}
+            <div style={{ marginBottom: 150 }}>{children}</div>
+
+            <Footer style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Button
+                        href='https://github.com/saad696/url-shortener_client'
+                        target='_blank'
+                        rel='noreferrer'
+                        type='link'
+                        icon={<GithubFilled />}
+                    >
+                        Frontend Source Code
+                    </Button>
+                    <span>|</span>
+                    <Button
+                        href='https://github.com/saad696/url-shortener_server'
+                        target='_blank'
+                        rel='noreferrer'
+                        type='link'
+                        icon={<GithubFilled />}
+                    >
+                        Backend Source Code
+                    </Button>
+                </div>
+                <div style={{ textAlign: 'center' }}>
+                    <p>
+                        Made with 🧠 by{' '}
+                        <a
+                            href='https://saadshaikh.netlify.app'
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            Saad Shaikh
+                        </a>
+                    </p>
+                </div>
+            </Footer>
         </>
     );
 };
